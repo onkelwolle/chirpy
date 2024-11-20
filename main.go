c0180d40c4834afe715a58d00087a22c9d657f8d
Wolfgang Zimmermann <wolfgang@zimmermann.red>
2024-11-20 17:15:52 +0100

    feat: add metrics endpoint, count the number of calls to the fileserver

--- main.go
+++ main.go
@@ -3,17 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
+	apiCfg := &apiConfig{}
+
 	fileServer := http.FileServer(http.Dir("."))
-	mux.Handle("/app/", http.StripPrefix("/app/", fileServer))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", fileServer)))
+
+	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("/reset", apiCfg.resetMetricsHandler)
 
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
 		w.Header().Add("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
 
@@ -27,5 +33,27 @@ func main() {
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 	}
+}
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	hits := cfg.fileserverHits.Load() // Atomically read the counter
+	fmt.Fprintf(w, "Hits: %d\n", hits)
+}
+
+func (cfg *apiConfig) resetMetricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	oldHits := cfg.fileserverHits.Swap(0) // Atomically reset the counter
+	fmt.Fprintf(w, "Resetting metrics. Previous count: %d\n", oldHits)
 }
